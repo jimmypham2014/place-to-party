6e088a4548444299a153f805842f393f9035a79f
jimmy <jimmyfam96@gmail.com>
2022-09-08 20:49:18 -0700

    dev branch make sure to merge with user auth later

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -46,6 +46,17 @@ app.use(routes); // Connect all the routes
 
 // backend/app.js
 // ...
+// Catch unhandled requests and forward to error handler.
+// backend/app.js
+
+app.use((_req, _res, next) =>{
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource not found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err)
+});
+// ...
 
 const port = process.env.PORT || 8000
 
